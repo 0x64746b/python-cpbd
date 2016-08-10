8c415f9a2ea5dc0b33df75452a969b01f16e3024
dtk <dtk@gmx.de>
2016-08-10 17:00:56 +0200

    Use `scipy.ndimage.imread()`
    Since the `Canny` algorithm used in the MatLab reference implementation uses a
    proprietary threshold auto-tuning algorithm, the results of this
    reimplementation will never match perfectly.
    
    Therefore minimal variations in intensities due to the application of the ITU-R
    601-2 luma transform are negligible.

--- cpbd/compute.py
+++ cpbd/compute.py
@@ -5,30 +5,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 from sys import argv
 
 import numpy as np
-from PIL import Image
-
-
-MATLAB_GRAYSCALE_MATRIX = (0.2989, 0.5870, 0.1140, 0)
-
-
-def imread(image_path):
-    # type: (str) -> numpy.ndarray
-    """
-    Open the image at the given path.
-
-    Convert it to grayscale and return an appropriately shaped array of
-    intensities.
-    """
-    image = Image.open(image_path)
-
-    # convert to gray scale if color image
-    if image.mode != 'L':
-        image = image.convert('L', MATLAB_GRAYSCALE_MATRIX)
-
-    # convert the image to double for further processing
-    return np.array(image.getdata(), dtype=np.float64).reshape(
-        image.size[::-1]
-    )
+from scipy.ndimage import imread
 
 
 def compute(input_image):
@@ -40,5 +17,5 @@ def compute(input_image):
 
 
 if __name__ == '__main__':
-    input_image = imread(argv[1])
+    input_image = imread(argv[1], mode='L')
     compute(input_image)
