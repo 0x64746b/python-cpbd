b83dfc989f467e9424463647af16025976c329d2
dtk <dtk@gmx.de>
2016-08-11 23:07:13 +0200

    Implement Octave's thinning method
    Matlab's [edge detection](http://www.mathworks.com/help/images/ref/edge.html)
    with the sobel method returns a thinned binary representation.

--- cpbd/compute.py
+++ cpbd/compute.py
@@ -6,6 +6,8 @@ from sys import argv
 
 import numpy as np
 from scipy.ndimage import imread
+from skimage.feature import canny
+from skimage.filters import sobel_v
 
 
 def compute(input_image):
@@ -14,6 +16,34 @@ def compute(input_image):
     print(input_image[0,0])
 
     width_jnb = np.concatenate([5*np.ones(51), 3*np.ones(205)])
+
+    canny_edges = canny(input_image)
+    sobel_edges = _simple_thinning(sobel_v(input_image))
+
+
+def _simple_thinning(strength):
+    # type: (numpy.ndarray) -> numpy.ndarray
+    """
+    Perform a very simple thinning.
+
+    Inspired by the [Octave implementation](https://sourceforge.net/p/octave/image/ci/default/tree/inst/edge.m#l512).
+    """
+    num_rows, num_cols = strength.shape
+
+    zero_column = np.zeros((num_rows, 1))
+    zero_row = np.zeros((1, num_cols))
+
+    x = (
+        (strength > np.concatenate((zero_column, strength[:, :-1]), axis=1)) &
+        (strength > np.concatenate((strength[:, 1:], zero_column), axis=1))
+    )
+
+    y = (
+        (strength > np.concatenate((zero_row, strength[:-1, :]))) &
+        (strength > np.concatenate((strength[1:, :], zero_row)))
+    )
+
+    return x | y
 
 
 if __name__ == '__main__':
