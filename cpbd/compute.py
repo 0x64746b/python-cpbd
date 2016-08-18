6dc4026c83f67e0419e6b6018a77f98d1848a21f
dtk <dtk@gmx.de>
2016-08-18 19:21:15 +0200

    Rename variables in `marziliano_method()`
    Human readable names revealed error in image boundary check.

--- cpbd/compute.py
+++ cpbd/compute.py
@@ -39,72 +39,90 @@ def marziliano_method(edges, image):
     :return: A matrix with the same dimensions as the given image with 0's at
         non-edge locations and edge-widths at the edge locations.
     """
+
+    # `edge_widths` consists of zero and non-zero values. A zero value
+    # indicates that there is no edge at that position and a non-zero value
+    # indicates that there is an edge at that position and the value itself
+    # gives the edge width.
     edge_widths = np.zeros(image.shape)
 
-    # TODO: Check weird order. Rename?
-    gy, gx = np.gradient(image)
+    # find the gradient for the image
+    gradient_y, gradient_x = np.gradient(image)
 
+    # dimensions of the image
     img_height, img_width = image.shape
 
+    # holds the angle information of the edges
     edge_angles = np.zeros(image.shape)
 
-    for m in range(img_height):
-        for n in range(img_width):
-            if gx[m, n] != 0:
-                edge_angles[m, n] = atan2(gy[m, n], gx[m, n]) * (180 / pi)
-            elif gx[m, n] == 0 and gy[m, n] == 0:
-                edge_angles[m,n] = 0
-            elif gx[m, n] == 0 and gy[m, n] == pi/2:
-                edge_angles[m, n] = 90
+    # calculate the angle of the edges
+    for row in range(img_height):
+        for col in range(img_width):
+            if gradient_x[row, col] != 0:
+                edge_angles[row, col] = atan2(gradient_y[row, col], gradient_x[row, col]) * (180 / pi)
+            elif gradient_x[row, col] == 0 and gradient_y[row, col] == 0:
+                edge_angles[row,col] = 0
+            elif gradient_x[row, col] == 0 and gradient_y[row, col] == pi/2:
+                edge_angles[row, col] = 90
+
 
     if np.any(edge_angles):
+
+        # quantize the angle
         quantized_angles = 45 * np.round(edge_angles / 45)
 
-        for m in range(1, img_height - 1):
-            for n in range(1, img_width - 1):
-                if edges[m, n] == 1:
+        for row in range(1, img_height - 1):
+            for col in range(1, img_width - 1):
+                if edges[row, col] == 1:
 
-                    if quantized_angles[m, n] == 180 or quantized_angles[m, n] == -180:
-                        for k in range(100 + 1):
-                            pos_y1 = (n - 1) - k
-                            pos_y2 = (n - 2) - k
+                    # gradient angle = 180 or -180
+                    if quantized_angles[row, col] == 180 or quantized_angles[row, col] == -180:
+                        for margin in range(100 + 1):
+                            inner_border = (col - 1) - margin
+                            outer_border = (col - 2) - margin
 
-                            if pos_y2 < 0 or (image[m, pos_y2] - image[m, pos_y1]) <= 0:
+                            # outside image or intensity increasing from left to right
+                            if outer_border < 0 or (image[row, outer_border] - image[row, inner_border]) <= 0:
                                 break
 
-                        width_count_side_1 = k + 1
+                        width_left = margin + 1
 
-                        for k in range(100 + 1):
-                            neg_y1 = (n + 1) + k
-                            neg_y2 = (n + 2) + k
+                        for margin in range(100 + 1):
+                            inner_border = (col + 1) + margin
+                            outer_border = (col + 2) + margin
 
-                            if neg_y2 >= img_height or (image[m, neg_y2] - image[m, neg_y1]) >= 0:
+                            # outside image or intensity increasing from left to right
+                            if outer_border >= img_width or (image[row, outer_border] - image[row, inner_border]) >= 0:
                                 break
 
-                        width_count_side_2 = k + 1
+                        width_right = margin + 1
 
-                        edge_widths[m, n] = width_count_side_1 + width_count_side_2
+                        edge_widths[row, col] = width_left + width_right
 
-                    if quantized_angles[m, n] == 0:
-                        for k in range(100 + 1):
-                            pos_y1 = (n + 1) + k
-                            pos_y2 = (n + 2) + k
 
-                            if pos_y2 >= img_width or (image[m, pos_y2] - image[m, pos_y1]) <= 0:
+                    # gradient angle = 0
+                    if quantized_angles[row, col] == 0:
+                        for margin in range(100 + 1):
+                            inner_border = (col + 1) + margin
+                            outer_border = (col + 2) + margin
+
+                            # outside image or intensity decreasing from left to right
+                            if outer_border >= img_width or (image[row, outer_border] - image[row, inner_border]) <= 0:
                                 break
 
-                        width_count_side_1 = k + 1
+                        width_right = margin + 1
 
-                        for k in range(100 + 1):
-                            neg_y1 = (n - 1) - k
-                            neg_y2 = (n - 2) - k
+                        for margin in range(100 + 1):
+                            inner_border = (col - 1) - margin
+                            outer_border = (col - 2) - margin
 
-                            if neg_y2 < 0 or (image[m, neg_y2] - image[m, neg_y1] >= 0):
+                            # outside image or intensity decreasing from left to right
+                            if outer_border < 0 or (image[row, outer_border] - image[row, inner_border]) >= 0:
                                 break
 
-                        width_count_side_2 = k + 1
+                        width_left = margin + 1
 
-                        edge_widths[m, n] = width_count_side_1 + width_count_side_2
+                        edge_widths[row, col] = width_right + width_left
 
     return edge_widths
 
